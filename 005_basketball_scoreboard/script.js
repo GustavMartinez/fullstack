a08442f2dbd12655667cad78b2860b7bd2243e7f
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-01 19:43:45 -0300

    basketball scoreboard

---
+++ 005_basketball_scoreboard/script.js
@@ -0,0 +1,20 @@
+let plusOne = 1;
+let plusTwo = 2;
+let plusThree = 3;
+let home = 0;
+let guest = 0;
+
+let homeEl = document.getElementById("home-el")
+let guestEl = document.getElementById("guest-el")
+
+
+
+function addOne(){
+    home = home + plusOne
+    homeEl.textContent = home;
+}
+
+function addTwo(){
+    home = home + plusTwo;
+    homeEl.textContent = home;
+}

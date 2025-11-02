0b80f980aa71888f6aece9a7362754643f64eeff
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-02 09:23:08 -0300

    stating logic of blackjack game

--- 006_blackjack_game/script.js
+++ 006_blackjack_game/script.js
@@ -1,5 +1,22 @@
 
-let firstCard = 2;
-let secondCard = 10;
+let firstCard = 11;
+let secondCard = 1;
 
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+
+if (sum <= 20){
+    console.log("Do you want to draw a new card?")
+
+}else if (sum === 21){
+    console.log("You've got Balckjack");
+    hasBlackJack = true
+}else if (sum > 21){
+    console.log("You're out of the game!")
+    isAlive = false;
+}
+
+console.log(hasBlackJack)
+console.log(isAlive)

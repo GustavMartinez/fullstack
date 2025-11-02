defc2063868ae08ff98194ffcb0971a393b3e38f
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-02 11:39:38 -0300

    changing functions and new button

--- 006_blackjack_game/script.js
+++ 006_blackjack_game/script.js
@@ -1,8 +1,11 @@
 let messageEL = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 let firstCard = 11;
-let secondCard = 1;
+let secondCard = 6;
+
+let cards = [firstCard, secondCard,]
 
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -10,11 +13,14 @@ let isAlive = true;
 
 let message = "";
 
-
-
 function startGame(){
+    renderGame();
+}
+
+function renderGame(){
 
     sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + cards[0] +" " + cards[1];
 
     if (sum <= 20){
         message = "Do you want to draw a new card?";
@@ -32,4 +38,12 @@ function startGame(){
     console.log(message)
     console.log(hasBlackJack)
     console.log(isAlive)
+}
+
+function newCard(){
+    console.log("Drawing a new card from the deck!")
+
+    let card = 5;
+    sum += card;
+    renderGame()
 }

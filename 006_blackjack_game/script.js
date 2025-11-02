c60c5b8cc5bb0c03b9e55ce44259c1d23417813e
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-02 10:15:10 -0300

    funcionalities of butoon and logic

--- 006_blackjack_game/script.js
+++ 006_blackjack_game/script.js
@@ -1,3 +1,5 @@
+let messageEL = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
 
 let firstCard = 11;
 let secondCard = 1;
@@ -6,17 +8,28 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 
+let message = "";
 
-if (sum <= 20){
-    console.log("Do you want to draw a new card?")
 
-}else if (sum === 21){
-    console.log("You've got Balckjack");
-    hasBlackJack = true
-}else if (sum > 21){
-    console.log("You're out of the game!")
-    isAlive = false;
+
+function startGame(){
+
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum <= 20){
+        message = "Do you want to draw a new card?";
+
+    }else if (sum === 21){
+        message = "You've got Balckjack";
+        hasBlackJack = true
+    }else if (sum > 21){
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    messageEL.textContent = message
+
+    console.log(message)
+    console.log(hasBlackJack)
+    console.log(isAlive)
 }
-
-console.log(hasBlackJack)
-console.log(isAlive)

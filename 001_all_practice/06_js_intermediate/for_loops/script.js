77f8953655d369182063e974608560742595b021
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2025-11-12 07:59:17 -0300

    intermediate js

---
+++ 001_all_practice/06_js_intermediate/for_loops/script.js
@@ -0,0 +1,23 @@
+
+//        start       finish      step size
+
+//for ( let count = 1; count < 11; count +=1){
+//    console.log(count)
+//}
+
+//for ( let count = 2; count < 20; count += 3){
+//    console.log(count)
+//}
+
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here",
+    "Ok"
+]
+
+for(let i = 0; i<messages.length; i += 1){
+    console.log(messages[i])
+}
---
+++ 001_all_practice/06_js_intermediate/for_loops/script_2.js
@@ -0,0 +1,5 @@
+let cards = [7,3,9];
+
+for(let i = 0; i < cards.length; i +=1){
+    console.log(cards[i])
+}

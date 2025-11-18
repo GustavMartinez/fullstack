df7f9b3230939e1d35a2a5ccf6ee970d8b405ba9
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-18 20:57:34 -0300

    javascript recap

---
+++ 001_all_practice/06_js_intermediate/some_functions/script.js
@@ -0,0 +1,18 @@
+var firstNameLength = 0;
+var fristName = "Ada";
+
+firstNameLength = fristName.length;
+
+
+console.log(fristName)
+console.log(firstNameLength)
+
+
+let yo = "Gustavo";
+let yoLength = 0;
+
+
+yoLength = yo.length;
+
+console.log(yo)
+console.log(yoLength)

50c4b2ed091d542e19e61a4b7aebcff27c0e4e46
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-29 06:39:15 -0300

    array methods

---
+++ 001_all_practice/06_Js_basics/07_basic.js
@@ -0,0 +1,37 @@
+
+/* array functions:
+
+- .push()
+- .pop()
+- .shift()
+- .unshift()
+
+*/
+
+
+// ORIGINAL ARRAY
+var myNewArray = ["Spimsons", "H", "Dog"];
+console.log("the original array is: " + myNewArray)
+
+
+// PUSH FUNCTION
+myNewArray.push(["happy", "Joy"])
+myNewArray.push("Cat")
+
+console.log(myNewArray)
+
+
+// POP FUNCTION
+
+myNewArray.pop();
+console.log(myNewArray)
+
+
+// SHIFT FUNCTION
+myNewArray.shift();
+console.log(myNewArray)
+
+
+// UNSHIFT FUNCTION
+myNewArray.unshift("Leonidas")
+console.log(myNewArray)

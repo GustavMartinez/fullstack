e74ea7aa1f865f2307014a15074f6cd3f83f432b
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-29 07:05:10 -0300

    return in functions

---
+++ 001_all_practice/06_Js_basics/09_functions.js
@@ -0,0 +1,34 @@
+function minusSeven(num){
+    return num - 7;
+}
+
+console.log(minusSeven(15));
+
+function timesFive(num){
+    return num * 5;
+}
+
+console.log(timesFive(20));
+
+
+
+// ASSING THE RETURN VALUE
+
+var changed = 0;
+
+function change(num){
+    return (num + 5) / 3;
+}
+
+changed = change(10);
+console.log(changed)
+
+
+var processed = 0;
+
+function processArg(num){
+    return (num + 3) / 5;
+}
+
+processed = processArg(5);
+console.log(processed)

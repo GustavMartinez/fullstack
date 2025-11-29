9124c3572a890015e5b551d23a0d164a3dd626e7
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-11-29 06:51:58 -0300

    basic about functions in js

---
+++ 001_all_practice/06_Js_basics/08_functions.js
@@ -0,0 +1,35 @@
+// CREATE A FUNCTION:
+
+function myReusableFunction(){
+    console.log("Heyya, World");
+}
+
+
+myReusableFunction();
+
+
+// other function
+
+function reusableFunction(){
+    console.log("Hi, World");
+}
+
+
+reusableFunction()
+
+
+// Parameters
+
+function myNewFunction(a,b){
+    console.log(a + b)
+}
+
+myNewFunction(5, 6)
+
+
+function functionWithArgs(a, c){
+    console.log(a + c);
+}
+
+
+functionWithArgs(10,60)

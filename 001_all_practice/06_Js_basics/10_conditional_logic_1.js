77943ac6a59f2c76c51e4aebd8348f7014c78c39
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-08 10:53:15 -0300

    conditional logic in js practice

--- 001_all_practice/06_Js_basics/10_conditional_logic_1.js
+++ 001_all_practice/06_Js_basics/10_conditional_logic_1.js
@@ -14,7 +14,7 @@ console.log(trueOrFalse(true))
 // comparison
 
 function testEqual(val){
-    if (val == 12){
+    if (val == 12){4
         return "Equal";
     }
     else{
@@ -24,3 +24,28 @@ function testEqual(val){
 
 console.log(testEqual(12))
 console.log(testEqual("Hola"))
+
+// comparison and logical operators:
+
+// Logical operator AND &&
+function testLogical(val){
+    if (val <= 50 && val >= 25){
+        return "Yes";
+    }
+    else{
+        return "No";
+    }
+
+}
+
+console.log(testLogical(30));
+console.log(testLogical(80));
+
+
+// Logical operator OR ||
+
+function testLogicalOr(){
+    if (val < 10 || val > 0){
+        return "yes"
+    }
+}
---
+++ 001_all_practice/06_Js_basics/12_conditional_logic_3.js
@@ -0,0 +1,30 @@
+// Switch statement
+
+function caseInSwitch(val){
+    var answer = "";
+    switch(val){
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+        default:
+            answer = "not valid number";
+            break;
+    };
+
+    return answer;
+};
+
+console.log(caseInSwitch(1))
+console.log(caseInSwitch(2))
+console.log(caseInSwitch(3))
+console.log(caseInSwitch(4))
+console.log(caseInSwitch(5))

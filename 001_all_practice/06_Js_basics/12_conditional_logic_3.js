ce25cf1470abc96c35853572866eb2ae6992cc2b
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-08 13:26:01 -0300

    conditional logic with switch

--- 001_all_practice/06_Js_basics/12_conditional_logic_3.js
+++ 001_all_practice/06_Js_basics/12_conditional_logic_3.js
@@ -28,3 +28,32 @@ console.log(caseInSwitch(2))
 console.log(caseInSwitch(3))
 console.log(caseInSwitch(4))
 console.log(caseInSwitch(5))
+
+
+// switch statements with mutiple identical options:
+
+function sequentialSizes(val){
+    var answer2 = "";
+    switch(val){
+        case 1:
+        case 2:
+        case 3:
+            answer2 = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer2 = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer2 = "High";
+            break;
+    };
+    return answer2;
+};
+
+console.log(sequentialSizes(2))
+console.log(sequentialSizes(5))
+console.log(sequentialSizes(9))

a04b1fd80034d25dc5772e1333cec82e8e3150de
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-13 10:22:24 -0300

    funcionalidades de los botones en javascript

---
+++ 001_all_practice/08_counter/index.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment(){
+    count += 1;
+    countEl.innerHTML = count;
+}
+
+function save(){
+    let entries = count + " - ";
+    saveEl.textContent += entries;
+    count = 0;
+    countEl.innerHTML = count;
+}

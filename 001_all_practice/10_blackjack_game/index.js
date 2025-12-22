1cb9f144a632f2055d27f0be1e364077bbbf14e1
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-22 16:11:58 -0300

    adicion del boton de comenzar el juego, html, css y js, en proyecto de practica

--- 001_all_practice/10_blackjack_game/index.js
+++ 001_all_practice/10_blackjack_game/index.js
@@ -8,16 +8,19 @@ let isAlive = true;
 
 let message = "";
 
-if (sum <= 20){
-    message = "Do you want to draw a new card?";
-} 
-else if (sum === 21){
-    message = "You have got Blackjack!";
-    hasBlackJack = true
-}
-else {
-    message = "you are out of the game! ";
-    isAlive = false;
-}
 
-console.log(message)
+function startGame(){
+    if (sum <= 20){
+        message = "Do you want to draw a new card?";
+    } 
+    else if (sum === 21){
+        message = "You have got Blackjack!";
+        hasBlackJack = true
+    }
+    else {
+        message = "you are out of the game! ";
+        isAlive = false;
+    }
+
+    console.log(message)
+}

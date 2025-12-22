c0dd404727b7a3315c66299ce83cbcf63629af67
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-22 08:38:10 -0300

    blackjack game practice

---
+++ 001_all_practice/10_blackjack_game/index.js
@@ -0,0 +1,23 @@
+let firstCard = 10;
+let secondCard = 10;
+
+let sum = firstCard + secondCard;
+
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
+
+if (sum <= 20){
+    message = "Do you want to draw a new card?";
+} 
+else if (sum === 21){
+    message = "You have got Blackjack!";
+    hasBlackJack = true
+}
+else {
+    message = "you are out of the game! ";
+    isAlive = false;
+}
+
+console.log(message)
